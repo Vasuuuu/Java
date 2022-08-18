55de56130d7aca3435b45929ea9746a2950e5eda
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-18 20:53:52 +0530

    Create TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,17 @@
+public class TypeCasting{
+     public static void main(Strings[] args){
+       //Widening Casting
+         int myInt = 9;
+         double myDouble = myInt; 
+
+    System.out.println(myInt);      
+    System.out.println(myDouble);   
+  
+   /* Narrowing Casting
+      double myDouble = 9.78d;
+      int myInt = (int) myDouble;
+
+      System.out.println(myDouble);  
+      System.out.println(myInt);  */
+  }
+}
