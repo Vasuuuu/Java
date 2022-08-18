efeb89d145569bf1aa16b8ae30a6f10570c874a6
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-18 19:11:25 +0530

    Create CommandLine.java

---
+++ CommandLine.java
@@ -0,0 +1,7 @@
+public class CommandLine {
+     public static void main(String[] args){
+     for(int i = 0 ; i < args.length; i++){
+     system.out.println("args [" + i + "] : " + args[i]);
+     }
+   }
+} // displaying all command line arguments
