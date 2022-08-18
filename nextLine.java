8c62cac8ae017c24994f11c8b02b3fd485f4ae97
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-18 19:45:29 +0530

    Create nextLine.java

---
+++ nextLine.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class nextLine{
+     public static void main(String[] args){
+      Scanner input = new Scanner(System.in);
+      System.out.println("Type your name : ");
+      name = input.nextLine();
+      System.out.println("Your name is "+name);
+      }
+}
