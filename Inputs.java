a6b323bac42bb32e2c32edb990a036247451a6f7
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-18 19:33:23 +0530

    Create Inputs.java

---
+++ Inputs.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Inputs{
+     public static void main(String[] args){
+       Scanner input = new Scanner(System.in);
+       System.out.println("Enter your roll no : ");
+       int rollno = input.nextInt();
+       System.out.println("Your roll number is "+ rollno);
+
+   /*    String name = input.next();
+       System.out.println("Your name is "+name);
+       
+       float marks = input.nextFloat();
+       System.out.println("Your marks is "+marks); */
+     }
+}
