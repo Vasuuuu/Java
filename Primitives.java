ac8b9b98427f931290c9f44d8e3ced067f4d303b
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-18 20:12:10 +0530

    Create Primitives.java

---
+++ Primitives.java
@@ -0,0 +1,16 @@
+public class Primitives{
+     public static void main(String[] args){
+        int rollno = 7;
+        char letter = 'V';
+        float marks = 93.59f;
+        double largeDecimalNumbers = 12345678.5559;
+        long largeInteger = 123456789101112L;
+        boolean check = false;
+        
+        float c = 55.5f;
+        c++; 
+        // c = (int)(c)+1;
+        System.out.println(c);
+     }
+}
+        
