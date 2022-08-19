df605e10e4a118fa3c474ae79cc02b60ddae7489
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-19 23:58:06 +0530

    Update SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 public class SimpleInterest{
 public static void main(String[] args){
+Scanner input = new Scanner(System.in);
 float p, t, r;
-p= 10000;
-t= 2;
-r=5;
+System.out.print("Enter principal amount : ");
+p= input.nextFloat();
+System.out.print("Enter time in years : ");
+t = input.nextFloat();
+System.out.print("Enter rate of interest : ");
+r = input.nextFloat();
 SI=(p*t*r)/100;
 System.out.println("Simple Interest : "+SI);
 }
