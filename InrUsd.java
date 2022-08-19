5f3146a65297fd6e1bc68824fc10193755440f7b
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-20 00:54:13 +0530

    Create InrUsd.java

---
+++ InrUsd.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class InrUsd{
+public static void main(Strings[] args){
+Scanner input = new Scanner(System.in);
+float inr = input.nextFloat();
+float usd = inr*0.013;
+System.out.println(inr + " INR = "+usd+" USD.");
+}
+}
