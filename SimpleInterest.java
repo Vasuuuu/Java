7d6970206d40e3b2cd2d8e254475439eb9660af3
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-19 23:51:57 +0530

    Create SimpleInterest.java

---
+++ SimpleInterest.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class SimpleInterest{
+public static void main(String[] args){
+float p, t, r;
+p= 10000;
+t= 2;
+r=5;
+SI=(p*t*r)/100;
+System.out.println("Simple Interest : "+SI);
+}
+}
