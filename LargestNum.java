7650a26bfabac08ba16da8cdb7cdb47e90769c80
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-20 00:43:23 +0530

    Create LargestNum.java

---
+++ LargestNum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public claas LargestNum{
+public static void main(Strings[] args){
+int num1,num2,greatest;
+Scanner input = new Scanner(System.in);
+System.out.print("Enter 1st number : ");
+num1 = input.nextInt();
+System.out.print("Enter 2nd number : ");
+num2 = input.nextInt();
+if (num1>num2){
+greatest= num1;
+}else{
+greatest=num2;}
+System.out.println("Greatest number is : "+greatest);
+}
+}
