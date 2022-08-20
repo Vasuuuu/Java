f9f448edd1058cc17f282eddb428182131d668e4
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-20 10:35:17 +0530

    Create areaTriangle.java

---
+++ areaTriangle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class areaTriangle {
+  public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+    System.out.print("Enter length of base of triangle : ");
+    double b = input.nextDouble();
+    System.out.print("Enter length of height of triangle : ");
+double h = input.nextDouble();
+System.out.println("Area of triangle : "+(b*h)/2+"\n");
+  }
