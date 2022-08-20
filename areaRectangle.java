9863e83e92c38d8a9493331570d7388ae8998996
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-20 10:39:21 +0530

    Create areaRectangle.java

---
+++ areaRectangle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+    System.out.print("Enter length of rectangle : ");
+    double l = input.nextDouble();
+    System.out.print("Enter breadth of rectangle : ");
+double b = input.nextDouble();
+System.out.println("Area of rectangle : "+l*b+"\n");
+  }
+}
