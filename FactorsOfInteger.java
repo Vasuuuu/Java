566f16bb0da3e6fcf2068644632ffc02f0133854
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-21 19:55:18 +0530

    Create FactorsOfInteger.java

---
+++ FactorsOfInteger.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class FactorsOfInteger{
+public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a number : ");
+    int num = input.nextInt();
+    System.out.print("Factors of "+num+" are : ");
+    for(int i=1;i<=num;i++){
+        if(num%i==0){
+            System.out.print(i+" ");
+        }
+    }
+}
+}
