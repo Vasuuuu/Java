d12ae81d5e6cb5642db20daca59132239640d905
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-25 01:29:13 +0530

    Update and rename Sum2nums.java to SumPro2nums.java

--- Methods/SumPro2nums.java
+++ Methods/SumPro2nums.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
-public class Sum2nums{
+public class SumPro2nums{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter two numbers : ");
         int num1 = input.nextInt();
         int num2 = input.nextInt();
         System.out.println("Sum : "+sum(num1,num2));
+        System.out.println("\nProduct : "+pro(num1,num2));
     }
     static int sum(int num1, int num2){
         return num1+num2;
+    }
+    static int pro(int num1, int num2){
+        return num1*num2;
     }
 }
