6f057da068a63491fd3bddaa8c57b4f572117f62
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-25 01:25:27 +0530

    Create Sum2nums.java

---
+++ Methods/Sum2nums.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Sum2nums{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter two numbers : ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        System.out.println("Sum : "+sum(num1,num2));
+    }
+    static int sum(int num1, int num2){
+        return num1+num2;
+    }
+}
