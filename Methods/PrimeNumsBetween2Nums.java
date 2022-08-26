1b7f2f30e97e6c49ebab4ac6dbf66b95c601f6db
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-26 21:09:00 +0530

    Create PrimeNumsBetween2Nums.java

---
+++ Methods/PrimeNumsBetween2Nums.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class PrimeNumsBetween2Nums{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter lower bound of the interval : ");
+        int lower = input.nextInt();
+        System.out.print("Enter upper bound of the interval : ");
+        int upper = upper.nextInt();
+        for(i=lower;i<=upper;i++){
+            if(isPrime){
+                System.out.print(i+" ");
+            }
+        }
+    }
+    static boolean isPrime(int n){
+        
+    }
+}
