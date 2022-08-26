fcd7b4fa2bf3639ebea28ff6e7ae25c0693f9526
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-26 23:18:52 +0530

    Update PrimeNumsBetween2Nums.java

--- Methods/PrimeNumsBetween2Nums.java
+++ Methods/PrimeNumsBetween2Nums.java
@@ -5,14 +5,25 @@ public class PrimeNumsBetween2Nums{
         System.out.print("Enter lower bound of the interval : ");
         int lower = input.nextInt();
         System.out.print("Enter upper bound of the interval : ");
-        int upper = upper.nextInt();
-        for(i=lower;i<=upper;i++){
-            if(isPrime){
+        int upper = input.nextInt();
+        for(int i=lower;i<=upper;i++){
+            if(isPrime(i)){
                 System.out.print(i+" ");
             }
         }
     }
     static boolean isPrime(int n){
-        
+        if (n==0||n==1){
+            return false;
+        }
+        if(n==2){
+            return true;
+        }
+        for(int i = 2; i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
     }
 }
