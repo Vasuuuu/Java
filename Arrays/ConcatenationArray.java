340a8f4e4828d5e4e78cc561832e123e2cddb27d
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-29 23:44:46 +0530

    Create ConcatenationArray.java

---
+++ Arrays/ConcatenationArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class ConcatenationArray{
+public static int[] getConcatenation(int[] nums){
+        int n = nums.length;
+        int[] ans = new int[2*n];
+        for(int i = 0; i<n;i++){
+            ans[i]=nums[i];
+            ans[n+i]=nums[i];
+        }
+        return ans;
+    }
+    public static void main(String[] args){
+        int[] nums ={1,3,2,1};
+        System.out.println(Arrays.toString(getConcatenation(nums)));
+    }
+}
