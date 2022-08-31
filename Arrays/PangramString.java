e0816d36b084c8255f833b2769a2212c20224e2c
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-08-31 21:19:55 +0530

    Create PangramString.java

---
+++ Arrays/PangramString.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class PangramString{
+    public static void main(String[] args){
+        String str ="thequickbrownfoxjumpsoverthelazydog";
+        System.out.println("Pangram String : "+checkPangram(str));
+    }
+static boolean checkPangram(String str){
+    int[] arr = new int[26];
+    
+    
+}
+}
