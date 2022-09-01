8483b5bdebe695afe6baca39a90b206b999b6e6c
Vasuuuu <87427261+Vasuuuu@users.noreply.github.com>
2022-09-01 23:24:52 +0530

    Create TransposeMatrix.java

---
+++ TransposeMatrix.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class TransposeMatrix{
+    public static void main(String[] args){
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(Arrays.toString((TM(matrix))));
+    }
+    static int[][] TM(int[][] matrix){
+        
+    }
+}
